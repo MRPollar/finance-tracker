72d46aed668c702dc5753bee84abf6675fd45742
Alan Tavares Morais <pollargithub@gmail.com>
2024-05-25 13:59:10 -0300

    estrutura de projeto

---
+++ src/app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  titleTemplate:(title) => title ? `${title} | Finance` : 'Finance',
+})
+</script>
---
+++ src/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <Head>
+        <Title>Home</Title>
+    </Head>
+    <main>
+        <h1>hello</h1>
+    </main>
+</template>
